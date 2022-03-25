299695387934375e4b8fbc09cbf969ae7c084837
soyeon <bless0053@gmail.com>
2022-03-25 14:05:11 +0900

    Learning about prop
    Props allows the sharing of properties between objects as objects that contain component information.

--- Udemy/Section01.Start/section01/src/components/ExpenseItem.js
+++ Udemy/Section01.Start/section01/src/components/ExpenseItem.js
@@ -2,19 +2,24 @@ import './ExpenseItem.css';
 
 //리액트에서 컴포넌트는 단지 JSX코드를 리턴하는 함수 일 뿐이다
 
-function ExpenseItem(){
-    const expenseDate = new Date(2021,2,28).toISOString();
-    const expenseTitle = 'CarInsurance';
-    const expenseAmount = 294.81;
+function ExpenseItem(props){
     // 반드시 하나의 상위 root 요소를 가져야 한다.
     //{} 하면 기본 자바스클비트 표현을 쓸 수 있다. 
+    const month = props.date.toLocaleString('en-US',{month : 'long'});
+    const day = props.date.toLocaleString('en-US',{day : '2-digit'});
+    const year = props.date.getFullYear();
+
     return(
         <div className='expense-item'>
-            <div>{expenseDate}</div>
+            <div>
+                <div>{year}</div>
+                <div>{month}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>
-                    ${expenseAmount}
+                    ${props.amount}
                 </div>
             </div>
         </div>
