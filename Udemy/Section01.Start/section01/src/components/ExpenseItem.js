c8f0b366752742a836a7a84529211ce73fc3ed65
soyeon <bless0053@gmail.com>
2022-03-25 16:16:48 +0900

    props02
    Practice sending and receiving data
    by props after dividing components.

--- Udemy/Section01.Start/section01/src/components/ExpenseItem.js
+++ Udemy/Section01.Start/section01/src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 //리액트에서 컴포넌트는 단지 JSX코드를 리턴하는 함수 일 뿐이다
@@ -5,17 +6,9 @@ import './ExpenseItem.css';
 function ExpenseItem(props){
     // 반드시 하나의 상위 root 요소를 가져야 한다.
     //{} 하면 기본 자바스클비트 표현을 쓸 수 있다. 
-    const month = props.date.toLocaleString('en-US',{month : 'long'});
-    const day = props.date.toLocaleString('en-US',{day : '2-digit'});
-    const year = props.date.getFullYear();
-
     return(
         <div className='expense-item'>
-            <div>
-                <div>{year}</div>
-                <div>{month}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>
