8f2096dd3eaff8f957ff9338ec0a433afa967c81
soyeon <bless0053@gmail.com>
2022-03-30 01:21:59 +0900

    [Udemy] React03. condition Render
    create ExpensesList Component
    Separate components to make JSX easier to use

--- Udemy/Section03.Render/event/src/components/Expenses/Expenses.js
+++ Udemy/Section03.Render/event/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
-import ExpenseItem  from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import './Expenses.css'
 import {useState} from 'react';
 
@@ -14,6 +14,7 @@ function Expenses(props){
     const filterExpenses = props.items.filter(expense =>{
         return expense.date.getFullYear().toString() === filteredYear;
     });
+
     return(
         <div>
             <Card className='expenses'>
@@ -21,16 +22,7 @@ function Expenses(props){
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler}
                 />
-                {/* 두번째 인자로 index 넣는짓 하지마라 버그날 수도 있음*/}
-                {filterExpenses.map((expense) => (
-                    <ExpenseItem 
-                        // key는 리액트 개별아이템을 식별할 수 있도록 도와줌
-                            key={expense.id}
-                            title={expense.title}
-                            amount={expense.amount}
-                            date ={expense.date}
-                    />
-                ))}
+                <ExpensesList items={filterExpenses} />
             </Card>
         </div>
 
